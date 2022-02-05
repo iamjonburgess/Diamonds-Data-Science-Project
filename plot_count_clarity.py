b9ad2b0a4143cb0ac62a320e8ffe2f5668e9e315
Jon | Software Engineer (Python, Django, PostgreSQL) <56128717+iamjonburgess@users.noreply.github.com>
2022-02-05 00:13:50 -0500

    Create plot_count_clarity.py

---
+++ plot_count_clarity.py
@@ -0,0 +1,22 @@
+"""
+This program takes the raw data, creates a Dataframe,
+and creates a bar graph to show the number of diamonds in
+each clarity type.
+"""
+
+# Import libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read the diamonds CSV file and build a Dataframe from the data
+df = pd.read_csv('diamonds.csv')
+
+clarityindexes = df['clarity'].value_counts().index.tolist()
+claritycount = df['clarity'].value_counts().values.tolist()
+
+print(clarityindexes)
+print(claritycount)
+
+plt.bar(clarityindexes, claritycount)
+plt.show() # or plt.savefig('plot_count_clarity.png')
